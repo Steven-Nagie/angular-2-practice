c0187c6431539ab99eb4ce62455901fc93a42eee
Steven Nagie <sanagie@yahoo.com>
2017-04-19 17:22:35 -0600

    Got rid of error messages in Chrome, IE still not working

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //as element
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  //as attribute
+  // selector: '[app-server]',
+  //as class
+  // selector: '.app-server',
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white
+    }
+  `]
 })
 export class ServerComponent {
+  serverId: number = 12;
+  serverStatus: string = "offline";
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
